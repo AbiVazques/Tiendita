fc93cb094f202baef77fd9a0fa8f71370a5aa804
jalouFullStack <dylben544@gmail.com>
2026-05-26 07:43:31 -0600

    Commit en Dylan

--- tienditas/src/app/app.routes.ts
+++ tienditas/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { Dasboard } from './pages/dasboard/dasboard';
+import { Products } from './pages/products/products';
 
 export const routes: Routes = [
-    {
-        path: 'dashboard',
-        component: Dasboard
-    }
+  {
+    path: 'dashboard',
+    component: Dasboard,
+  },
+  {
+    path: 'products',
+    component: Products,
+  },
 ];
